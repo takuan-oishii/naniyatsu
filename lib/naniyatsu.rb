13cad49e93c0bb4603c554b0d8e93c33f63f8325
hisaki-kanno <hisaki.kanno@litalico.co.jp>
2018-10-16 20:51:02 +0900

    create Naniyatsu module

--- lib/naniyatsu.rb
+++ lib/naniyatsu.rb
@@ -1,5 +1,22 @@
-require "naniyatsu/version"
+require 'naniyatsu/version'
+require 'active_support/core_ext/string/inflections'
 
 module Naniyatsu
-  # Your code goes here...
+  attr_reader :root_naniyatsu_class
+
+  def self.extended(klass)
+    klass.instance_variable_set :@root_naniyatsu_class, klass
+    klass.send(:define_method, klass.naniyatsu_method_name(klass), -> { true })
+  end
+
+  def inherited(klass)
+    root_naniyatsu_class = klass.superclass.root_naniyatsu_class
+    klass.instance_variable_set :@root_naniyatsu_class, root_naniyatsu_class
+    klass.send(:define_method, naniyatsu_method_name(klass), -> { true })
+    root_naniyatsu_class.send(:define_method, naniyatsu_method_name(klass), -> { false })
+  end
+
+  def naniyatsu_method_name(klass)
+    klass.to_s.underscore + '?'
+  end
 end
