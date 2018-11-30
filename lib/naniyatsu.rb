498b8012640c1163bccf8036deec83d7fe0bb8af
hisaki-kanno <hisaki.kanno@litalico.co.jp>
2018-11-30 10:01:46 +0900

    add super to interited

--- lib/naniyatsu.rb
+++ lib/naniyatsu.rb
@@ -14,6 +14,7 @@ module Naniyatsu
     klass.instance_variable_set :@root_naniyatsu_class, root_naniyatsu_class
     klass.send(:define_method, naniyatsu_method_name(klass), -> { true })
     root_naniyatsu_class.send(:define_method, naniyatsu_method_name(klass), -> { false })
+    super
   end
 
   def naniyatsu_method_name(klass)
